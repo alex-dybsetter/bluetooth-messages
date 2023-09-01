dc73fa9dcf31f548a0a4418ed76dd31ea89493b7
Alex Blass <alex.blass@willowtreeapps.com>
2023-09-01 10:08:13 -0400

    chore: set up project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
+	id("kotlin-kapt")
+	id("kotlin-android")
+	id("com.google.dagger.hilt.android")
+	id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -40,7 +44,7 @@ android {
 		compose = true
 	}
 	composeOptions {
-		kotlinCompilerExtensionVersion = "1.4.3"
+		kotlinCompilerExtensionVersion = "1.5.1"
 	}
 	packaging {
 		resources {
@@ -51,19 +55,44 @@ android {
 
 dependencies {
 
-	implementation("androidx.core:core-ktx:1.9.0")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-	implementation("androidx.activity:activity-compose:1.7.2")
+	val accompanistPermissionsVersion = "0.30.1"
+	val androidxCoreTestingVersion = "2.2.0"
+	val androidxEspressoVersion = "3.5.1"
+	val androidxJunitVersion = "1.1.5"
+	val androidxVersion = "1.10.1"
+	val composeVersion = "1.7.2"
+	val hiltVersion = "2.48"
+	val junitVersion = "4.13.2"
+	val lifecycleVersion = "2.6.1"
+
+	implementation("androidx.core:core-ktx:$androidxVersion")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+	implementation("androidx.activity:activity-compose:$composeVersion")
 	implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
-	testImplementation("junit:junit:4.13.2")
-	androidTestImplementation("androidx.test.ext:junit:1.1.5")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+	// Hilt
+	implementation("com.google.dagger:hilt-android:$hiltVersion")
+	implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+	annotationProcessor("com.google.dagger:hilt-compiler:$hiltVersion")
+	kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+	kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+	// Permissions
+	implementation("com.google.accompanist:accompanist-permissions:$accompanistPermissionsVersion")
+
+	// Android test
+	androidTestImplementation("androidx.test.ext:junit:$androidxJunitVersion")
+	androidTestImplementation("androidx.test.espresso:espresso-core:$androidxEspressoVersion")
+	androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+	// Tests
+	testImplementation("junit:junit:$junitVersion")
+	testImplementation("androidx.arch.core:core-testing:$androidxCoreTestingVersion")
 }
---
+++ app/src/main/java/com/ajblass/bluetoothmessages/MyApp.kt
@@ -0,0 +1,7 @@
+package com.ajblass.bluetoothmessages
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application()
